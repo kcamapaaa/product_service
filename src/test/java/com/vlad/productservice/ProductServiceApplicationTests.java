0ab74384609cb294851753aa036baa03b84e60f6
kcamapaaa <emelin.vladislav.1996@gmail.com>
2024-02-04 22:50:26 +0300

    1) added tests with testcontainers lib

--- src/test/java/com/vlad/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/vlad/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,58 @@
 package com.vlad.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vlad.productservice.dto.ProductRequest;
+import com.vlad.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+    @Container
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() throws Exception {
+        ProductRequest iphone = getProductRequest();
+        String iphoneString = objectMapper.writeValueAsString(iphone);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(iphoneString))
+                .andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("iPhone 11")
+                .description("iPhone 11")
+                .price(BigDecimal.valueOf(1200))
+                .build();
     }
 
 }
